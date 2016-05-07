caf34f79cc3c301c50ea329978158e0547744a19
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-06 21:20:40 -0700

    A065413 with test.

---
+++ Tests/Miscellaneous/A065413Test.hs
@@ -0,0 +1,9 @@
+import Test.Hspec
+import Miscellaneous.A065413 (a065413)
+
+main :: IO ()
+main = hspec $
+  describe "A065413" $
+    it "correctly computes the first 20 elements" $
+      take 20 (map a065413 [1..]) `shouldBe` expectedValue where
+        expectedValue = [1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1]
