7ce5bf931de24bfca523008fad6f4d2de4544cd4
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-06 21:19:59 -0700

    A000196 with test.

---
+++ HelperSequences/A000196.hs
@@ -0,0 +1,4 @@
+module HelperSequences.A000196 (a000196) where
+
+  a000196 :: Float -> Int
+  a000196 n = floor $ sqrt n
---
+++ Tests/HelperSequences/A000196Test.hs
@@ -0,0 +1,9 @@
+import Test.Hspec
+import HelperSequences.A000196 (a000196)
+
+main :: IO ()
+main = hspec $
+  describe "A000196" $
+    it "correctly computes the first 20 elements" $
+      take 20 (map a000196 [0..]) `shouldBe` expectedValue where
+        expectedValue = [0,1,1,1,2,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4]
