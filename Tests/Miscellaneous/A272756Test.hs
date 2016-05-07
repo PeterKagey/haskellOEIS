734624907ba00fe8ac4f21a5a76e383fcd2269d9
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-06 21:22:33 -0700

    A272756 with test.

---
+++ Tests/Miscellaneous/A272756Test.hs
@@ -0,0 +1,9 @@
+import Test.Hspec
+import Miscellaneous.A272756 (a272756)
+
+main :: IO ()
+main = hspec $
+  describe "A272756" $
+    it "correctly computes the first 20 elements" $
+      take 20 (map a272756 [1..]) `shouldBe` expectedValue where
+        expectedValue = [3,5,5,6,6,7,7,7,7,8,8,8,8,8,8,9,9,9,9,9]
