53a5918f6073e7cef90678e669187b78ffa8ffe5
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-06 21:46:22 -0700

    Rename function in EKGBuilder

--- Helpers/EKGBuilder.hs
+++ Helpers/EKGBuilder.hs
@@ -4,12 +4,11 @@ import Data.List (find, (\\))
 import Data.Maybe (fromJust)
 
 buildEKG :: [Integer] -> [Integer]
-buildEKG seed = a064413_list where
-  a064413_list = seed ++ remaining_list (length seed) where
+buildEKG seed = ekg_list where
+  ekg_list = seed ++ remaining_list (length seed) where
     remaining_list k = next_term : remaining_list (k + 1) where
       next_term = fromJust $ find nonCoprime unrepresentedInts where
         -- unrepresentedInts is the performance bottle-neck.
-        unrepresentedInts = [1..] \\ beginningOfList
-        beginningOfList = take k a064413_list
+        unrepresentedInts = [1..] \\ take k ekg_list
         nonCoprime i = gcd lastTerm i /= 1 where
-          lastTerm = a064413_list !! (k - 1)
+          lastTerm = ekg_list !! (k - 1)
