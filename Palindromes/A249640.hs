da4ff90b04125dd43cb17fe795753f7d1e326c59
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-08 17:32:42 -0600

    A249640 with test.

---
+++ Palindromes/A249640.hs
@@ -0,0 +1,8 @@
+module Palindromes.A249640 (a249640) where
+  import Helpers.PalindromeCounter (countPalindromes)
+
+  a249640 :: Int -> Integer
+  a249640 n = a249640_list !! n
+
+  a249640_list :: [Integer]
+  a249640_list = countPalindromes 7
