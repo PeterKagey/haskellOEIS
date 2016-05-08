565b3168db58d0ad1d182e59ec551de31b665402
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-08 13:50:56 -0700

    A269427 with test.

---
+++ AlecSequences/A269427.hs
@@ -0,0 +1,16 @@
+module AlecSequences.A269427 (a269427) where
+import Helpers.AlecHelper (buildAlecSequence)
+
+a269427 :: Int -> Integer
+a269427 i = a269427_list !! (i - 1)
+
+a269427_list :: [Integer]
+a269427_list = buildAlecSequence matchingIndices reducer [1]
+
+matchingIndices :: [Integer] -> [Int]
+matchingIndices list = filter f [1..n] where
+  n = length list
+  f index = (a269427 index - toInteger n - 1) `mod` toInteger index == 0
+
+reducer :: [Integer] -> Integer
+reducer seed = toInteger $ length seed
