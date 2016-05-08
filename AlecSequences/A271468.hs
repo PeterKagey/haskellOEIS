eb8d31f9a3ed910b64b8eafd582a82d7e272a0b9
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-08 12:19:18 -0700

    A271468 with test.

---
+++ AlecSequences/A271468.hs
@@ -0,0 +1,9 @@
+module AlecSequences.A271468 (a271468) where
+import AlecSequences.A271328 (a271328)
+
+a271468 :: Int -> Integer
+a271468 n = a271468_list !! (n - 1)
+
+a271468_list :: [Integer]
+a271468_list = map toInteger $ filter f [1..] where
+  f i = (a271328 i) /= toInteger (i * i + 1)
---
+++ Tests/AlecSequences/A271468Test.hs
@@ -0,0 +1,9 @@
+import Test.Hspec
+import AlecSequences.A271468 (a271468)
+main :: IO ()
+
+main = hspec $ do
+  describe "A271468" $ do
+    it "correctly computes the first 20 elements" $ do
+      take 20 (map a271468 [1..]) `shouldBe` expectedValue where
+        expectedValue = [1,5,10,15,17,20,25,28,30,34,35,37,40,45,50,51,55,56,60,65]
