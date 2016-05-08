f32d1c61f99a3b59bb44131b4dbdb92338b6d68d
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-08 12:30:04 -0700

    A096216 with test.

---
+++ AlecSequences/A096216.hs
@@ -0,0 +1,16 @@
+module AlecSequences.A096216 (a096216) where
+import Helpers.AlecHelper (buildAlecSequence)
+
+a096216 :: Int -> Integer
+a096216 n = a096216_list !! (n - 1)
+
+a096216_list :: [Integer]
+a096216_list = buildAlecSequence matchingIndices reducer [1]
+
+matchingIndices :: [Integer] -> [Int]
+matchingIndices list = filter f [1..n] where
+  n = length list
+  f index = gcd (toInteger n + 1) (list !! (index - 1)) == 1
+
+reducer :: [Integer] -> Integer
+reducer seed = toInteger $ length seed
---
+++ Tests/AlecSequences/A096216Test.hs
@@ -0,0 +1,9 @@
+import Test.Hspec
+import AlecSequences.A096216 (a096216)
+main :: IO ()
+
+main = hspec $ do
+  describe "A096216" $ do
+    it "correctly computes the first 20 elements" $ do
+      take 20 (map a096216 [1..]) `shouldBe` expectedValue where
+        expectedValue = [1,1,2,2,4,2,6,2,7,3,10,3,12,4,9,6,16,3,18,7]
