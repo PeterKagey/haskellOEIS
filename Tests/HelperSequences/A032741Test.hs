c3f5410666d90b8f2e7452895e8a6f181e99b31e
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-08 15:57:58 -0600

    A032741 with test.

---
+++ Tests/HelperSequences/A032741Test.hs
@@ -0,0 +1,9 @@
+import Test.Hspec
+import HelperSequences.A032741 (a032741)
+
+main :: IO ()
+main = hspec $
+  describe "A032741" $
+    it "correctly computes the first 10 elements" $
+      take 10 (map a032741 [0..]) `shouldBe` expectedValue where
+        expectedValue = [0,0,1,1,2,1,3,1,3,2]
