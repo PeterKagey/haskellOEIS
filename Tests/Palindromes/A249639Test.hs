11b698ff374634f5c3e7d56d0e8b804ccf060c8b
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-08 17:32:35 -0600

    A249639 with test.

---
+++ Tests/Palindromes/A249639Test.hs
@@ -0,0 +1,9 @@
+import Test.Hspec
+import Palindromes.A249639 (a249639)
+
+main :: IO ()
+main = hspec $
+  describe "A249639" $
+    it "correctly computes the first 10 elements" $
+      take 10 (map a249639 [0..]) `shouldBe` expectedValue where
+        expectedValue = [0,0,6,66,426,2706,16386,99186,595986,3580986]
