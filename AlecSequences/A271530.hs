da2a674d52c729f827e6e4b6dbc62d5ac228c5c5
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-08 16:43:50 -0600

    A271530 with test.

---
+++ AlecSequences/A271530.hs
@@ -0,0 +1,17 @@
+module AlecSequences.A271530 (a271530) where
+import Helpers.AlecHelper (buildAlecSequence)
+
+a271530 :: Int -> Integer
+a271530 i = a271530_list !! (i - 1)
+
+a271530_list :: [Integer]
+a271530_list = buildAlecSequence matchingIndices reducer [1]
+
+matchingIndices :: [Integer] -> [Int]
+matchingIndices list = filter f [1..n] where
+  n = length list
+  f index = ((toInteger n + 1) - a_i) `mod` toInteger(index) == 0 where
+    a_i = list !! (index - 1)
+
+reducer :: [Integer] -> Integer
+reducer seed = (foldr (*) 1 seed)
