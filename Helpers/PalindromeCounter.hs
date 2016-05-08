3b0b6855824e62b64506af0f76fad9d61ea7f5c1
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-08 17:33:53 -0600

    PalindromeCounter helper (required for last few commits.)

---
+++ Helpers/PalindromeCounter.hs
@@ -0,0 +1,11 @@
+module Helpers.PalindromeCounter (countPalindromes) where
+
+-- import Data.List (find, (\\))
+-- import Data.Maybe (fromJust)
+
+countPalindromes :: Int -> [Integer]
+countPalindromes b = count where
+  count = [0, 0] ++ remainder 1 where
+    remainder n = nextTerm : remainder (n + 1) where
+      nextTerm = (toInteger b) * (count !! n) + (toInteger b) ^ k - (count !! k)
+      k = 1 + (n `div` 2)
