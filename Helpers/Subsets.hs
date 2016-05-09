febce8f10085f222150c5e804dfa38f0f9d6c958
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-08 20:04:25 -0600

    Subset helper.

---
+++ Helpers/Subsets.hs
@@ -0,0 +1,10 @@
+module Helpers.Subsets (oneIndexed, zeroIndexed) where
+
+oneIndexed :: Integer -> [Int]
+oneIndexed n = map (1 +) (zeroIndexed n)
+
+zeroIndexed :: Integer -> [Int]
+zeroIndexed n = recurse n 0 [] where
+  recurse 0 c accum = accum
+  recurse n c accum = if odd n then recurse (n `div` 2) (c + 1) (c : accum) else recurse (n `div` 2) (c + 1) accum where
+
