ea8716e9efce0ced69e25da54a8669e951cbb7c2
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-08 20:11:57 -0600

    Change subset return type.

--- Helpers/Subsets.hs
+++ Helpers/Subsets.hs
@@ -1,9 +1,9 @@
 module Helpers.Subsets (oneIndexed, zeroIndexed) where
 
-oneIndexed :: Integer -> [Int]
+oneIndexed :: Integer -> [Integer]
 oneIndexed n = map (1 +) (zeroIndexed n)
 
-zeroIndexed :: Integer -> [Int]
+zeroIndexed :: Integer -> [Integer]
 zeroIndexed n = recurse n 0 [] where
   recurse 0 c accum = accum
   recurse n c accum = if odd n then recurse (n `div` 2) (c + 1) (c : accum) else recurse (n `div` 2) (c + 1) accum where
--- Subsets/A096111.hs
+++ Subsets/A096111.hs
@@ -2,4 +2,4 @@ module Subsets.A096111 (a096111) where
   import Helpers.Subsets (oneIndexed)
 
   a096111 :: Integer -> Integer
-  a096111 n = foldr (*) 1 (map toInteger (oneIndexed (n + 1)))
+  a096111 n = foldr (*) 1 $ oneIndexed $ n + 1
