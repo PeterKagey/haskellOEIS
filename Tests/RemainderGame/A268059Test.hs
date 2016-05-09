98d7ffb3542d364b2454a329555c2e9b6b3726db
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-08 21:16:16 -0600

    A268059 with test.

---
+++ Tests/RemainderGame/A268059Test.hs
@@ -0,0 +1,9 @@
+import Test.Hspec
+import RemainderGame.A268059 (a268059)
+
+main :: IO ()
+main = hspec $
+  describe "A268059" $
+    it "correctly computes the first 20 elements" $
+      take 20 (map a268059 [1..]) `shouldBe` expectedValue where
+        expectedValue = [1,2,1,1,1,2,2,1,2,2,1,1,1,1,4,4,2,1,1,1]
