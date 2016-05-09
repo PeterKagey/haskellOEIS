4b12841471178bd7ebc3b94f3ed23b54f8c19420
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-08 20:43:02 -0600

    A252698 with test.

---
+++ Tests/Nonpalindromes/A252698Test.hs
@@ -0,0 +1,9 @@
+import Test.Hspec
+import Nonpalindromes.A252698 (a252698)
+
+main :: IO ()
+main = hspec $
+  describe "A252698" $
+    it "correctly computes the first 10 elements" $
+      take 10 (map a252698 [0..]) `shouldBe` expectedValue where
+        expectedValue = [0,5,20,80,380,1820,9020,44720,223220,1114280]
