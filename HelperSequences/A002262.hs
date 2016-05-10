a8eb914673133e4d61ec45dfdb377cc56da2c75f
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-10 17:54:27 -0400

    Refactor for more honest signatures.

--- HelperSequences/A002262.hs
+++ HelperSequences/A002262.hs
@@ -2,5 +2,6 @@ module HelperSequences.A002262 (a002262) where
 import HelperSequences.A000217 (a000217)
 import HelperSequences.A003056 (a003056)
 
-a002262 :: Int -> Integer
-a002262 n = toInteger n - (a000217 $ toInteger $ a003056 n)
+a002262 :: Int -> Int
+-- a000217(a003056(n)) < n, so fromIntegral is safe.
+a002262 n = n - (fromIntegral $ a000217 $ toInteger $ a003056 n)
