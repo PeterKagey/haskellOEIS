73b352bda6d322cf7bea591b48347c668e7b3620
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-14 18:16:13 -0500

    Clean up warnings in tests.

--- Tests/AlecSequences/A269347Test.hs
+++ Tests/AlecSequences/A269347Test.hs
@@ -1,12 +1,10 @@
 module Tests.AlecSequences.A269347Test where
 import Test.Hspec
-import Test.QuickCheck
-import Control.Exception (evaluate)
 import AlecSequences.A269347 (a269347)
 main :: IO ()
 
-main = hspec $ do
-  describe "A269347" $ do
-    it "correctly computes the first 20 elements" $ do
+main = hspec $
+  describe "A269347" $
+    it "correctly computes the first 20 elements" $
       take 20 (map a269347 [1..]) `shouldBe` expectedValue where
         expectedValue = [1,1,3,3,3,15,3,3,30,3,3,51,3,3,84,3,3,111,3,3]
--- Tests/AlecSequences/A269423Test.hs
+++ Tests/AlecSequences/A269423Test.hs
@@ -1,12 +1,10 @@
 module Tests.AlecSequences.A269423Test where
 import Test.Hspec
-import Test.QuickCheck
-import Control.Exception (evaluate)
 import AlecSequences.A269423 (a269423)
 main :: IO ()
 
-main = hspec $ do
-  describe "A269423" $ do
-    it "correctly computes the first 20 elements" $ do
+main = hspec $
+  describe "A269423" $
+    it "correctly computes the first 20 elements" $
       take 20 (map a269423 [1..]) `shouldBe` expectedValue where
         expectedValue = [1,1,3,1,7,4,8,8,10,16,3,9,7,12,13,25,12,4,12,14]
