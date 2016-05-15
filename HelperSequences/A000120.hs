408a640e15ffc7776996fc11e042b616414b96a2
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-15 09:40:04 -0500

    Refactor A000120 to avoid inline if-else.

--- HelperSequences/A000120.hs
+++ HelperSequences/A000120.hs
@@ -3,4 +3,7 @@ import Data.Bits ((.&.), shiftR)
 
 a000120 :: Int -> Int
 a000120 n = go n 0 where
-  go k accum = if k == 0 then accum else go (shiftR k 1) $ (+) accum $ k .&. 1
+  go 0 accum = accum
+  go k accum = go k' accum'  where
+    k' = shiftR k 1
+    accum' = k .&. 1 + accum
