fc5ed6f895591c41ea23d4fbddff64cdc9405bb5
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-16 12:34:08 -0700

    A263135 with test.

---
+++ Tests/Coins/A263135Test.hs
@@ -0,0 +1,10 @@
+module Tests.Coins.A263135Test where
+import Test.Hspec
+import Coins.A263135 (a263135)
+
+main :: IO ()
+main = hspec $
+  describe "A263135" $
+    it "correctly computes the first 20 elements" $
+      map a263135 [0..20] `shouldBe` expectedValue where
+        expectedValue = [0,0,1,2,3,4,6,7,8,9,11,12,13,15,16,17,19,20,21,23,24]
