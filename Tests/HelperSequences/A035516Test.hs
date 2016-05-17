9a0294e720d90792b38159997676a8d3f4f38c72
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-17 09:52:04 -0700

    A035516 with test.

---
+++ Tests/HelperSequences/A035516Test.hs
@@ -0,0 +1,10 @@
+module Tests.HelperSequences.A035516Test where
+import Test.Hspec
+import HelperSequences.A035516 (a035516)
+
+main :: IO ()
+main = hspec $
+  describe "A035516" $
+    it "correctly computes the first 20 elements" $
+      take 20 (map a035516 [0..]) `shouldBe` expectedValue where
+        expectedValue = [0,1,2,3,3,1,5,5,1,5,2,8,8,1,8,2,8,3,8,3]
