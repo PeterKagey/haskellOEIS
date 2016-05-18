de8e91b631ee5b2fa903ce103a3e4bb324d46821
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-18 16:44:41 -0700

    Change signature of A000188.

--- HelperSequences/A000188.hs
+++ HelperSequences/A000188.hs
@@ -4,5 +4,5 @@ import Helpers.Primes (primePowers)
 -- (1) Number of solutions to x^2 = 0 (mod n). (2) Also square root of largest
 -- square dividing n. (3) Also Max_{ d divides n } GCD(d, n/d).
 
-a000188 :: Int -> Int
+a000188 :: Integer -> Integer
 a000188 n = product $ map (\t -> fst t ^ (snd t `div` 2)) $ primePowers n
