122040c5c83fb8cc22c648ba198ac134f9f7972a
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-18 16:45:09 -0700

    Refactor A096111 for function composition.

--- Subsets/A096111.hs
+++ Subsets/A096111.hs
@@ -2,4 +2,4 @@ module Subsets.A096111 (a096111) where
 import Helpers.Subsets (oneIndexed)
 
 a096111 :: Integer -> Integer
-a096111 n = product $ oneIndexed $ n + 1
+a096111 = product . oneIndexed . (1 +)
