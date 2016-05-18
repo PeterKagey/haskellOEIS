2221e3f9e13816f051d844fa879f737c30dfca67
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-18 08:11:12 -0700

    A271984 with test.

---
+++ Tests/Subsets/A271984Test.hs
@@ -0,0 +1,10 @@
+module Tests.Subsets.A271984Test where
+import Test.Hspec
+import Subsets.A271984 (a271984)
+
+main :: IO ()
+main = hspec $
+  describe "A271984" $
+    it "correctly computes the first 20 elements" $
+      take 20 (map a271984 [0..]) `shouldBe` expectedValue where
+        expectedValue = [34,35,36,37,38,39,44,45,46,47,50,51,52,53,54,55,60,61,62,63]
