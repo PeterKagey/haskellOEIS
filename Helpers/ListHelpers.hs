b80b8a78b2ec080112e4ce3fdcfccd53644951b5
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-19 15:40:11 -0700

    List helper: runLengths.

--- Helpers/ListHelpers.hs
+++ Helpers/ListHelpers.hs
@@ -1,4 +1,4 @@
-module Helpers.ListHelpers (concatReplicate, reciprocalSum) where
+module Helpers.ListHelpers (concatReplicate, reciprocalSum, runLengths) where
 -- concatReplicate is to replicate as concatMap is to map
 concatReplicate :: Int -> [a] -> [a]
 concatReplicate n list = take (n * length list) $ cycle list
@@ -7,3 +7,8 @@ concatReplicate n list = take (n * length list) $ cycle list
 --                     = 7 % 10
 reciprocalSum :: Integral a => [a] -> Rational
 reciprocalSum = sum . map (recip . toRational)
+
+runLengths :: Eq a => [a] -> [Int]
+runLengths [] = []
+runLengths l = h : runLengths (drop h l) where
+  h = length $ takeWhile (== head l) l
