45c58bd181b99639c26c8c320dc6f2a0720f64e8
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-20 12:06:55 -0700

    A002522 with test.

---
+++ Tests/HelperSequences/A002522Test.hs
@@ -0,0 +1,10 @@
+module Tests.HelperSequences.A002522Test where
+import Test.Hspec
+import HelperSequences.A002522 (a002522)
+main :: IO ()
+
+main = hspec $
+  describe "A002522" $
+    it "correctly computes the first 20 elements" $
+      take 20 (map a002522 [0..]) `shouldBe` expectedValue where
+        expectedValue = [1,2,5,10,17,26,37,50,65,82,101,122,145,170,197,226,257,290,325,362]
