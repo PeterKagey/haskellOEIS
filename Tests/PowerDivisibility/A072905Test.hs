c0269ea612cafd8471f5a3c46f85d7d7256d737e
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-20 13:56:01 -0700

    A072905 with test.

---
+++ Tests/PowerDivisibility/A072905Test.hs
@@ -0,0 +1,10 @@
+module Tests.PowerDivisibility.A072905Test where
+import Test.Hspec
+import PowerDivisibility.A072905 (a072905)
+main :: IO ()
+
+main = hspec $
+  describe "A072905" $
+    it "correctly computes the first 20 elements" $
+      take 20 (map a072905 [1..]) `shouldBe` expectedValue where
+        expectedValue = [4,8,12,9,20,24,28,18,16,40,44,27,52,56,60,25,68,32,76,45]
