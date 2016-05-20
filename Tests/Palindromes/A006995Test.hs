88c49cd3f1b32f63177002e1ab0ed83e53b3db93
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-20 12:25:44 -0700

    A009665 with test.

---
+++ Tests/Palindromes/A006995Test.hs
@@ -0,0 +1,10 @@
+module Tests.Palindromes.A006995Test where
+import Test.Hspec
+import Palindromes.A006995 (a006995)
+main :: IO ()
+
+main = hspec $
+  describe "A006995" $
+    it "correctly computes the first 20 elements" $
+      take 20 (map a006995 [1..]) `shouldBe` expectedValue where
+        expectedValue = [0,1,3,5,7,9,15,17,21,27,31,33,45,51,63,65,73,85,93,99]
