1b9b824f9213ddc73936f45771499991c6428ba1
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-20 13:15:03 -0700

    A269267 with test.

--- Tables/A107435.hs
+++ Tables/A107435.hs
@@ -1,7 +1,7 @@
 module Tables.A107435 (a107435) where
 import Helpers.Table (n'_k')
 
-a107435 :: Int -> Int
+a107435 :: Int -> Integer
 a107435 i = euclid 0 $ n'_k' (i - 1) where
   euclid c (_, 0) = c
   euclid c (a, b) = euclid (c + 1) (b, a `mod` b)
