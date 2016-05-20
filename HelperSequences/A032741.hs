f8f953350d860d089901c65983cd67b05fe2d51d
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-20 10:13:49 -0700

    Allow A032741 to take wider input.

--- HelperSequences/A032741.hs
+++ HelperSequences/A032741.hs
@@ -2,5 +2,5 @@ module HelperSequences.A032741 (a032741) where
 import Data.Bits ((.&.), shiftR)
 
 -- This is a slow proof of concept.
-a032741 :: Int -> Int
+a032741 :: Integer -> Int
 a032741 n = length $ filter (\k -> n `mod` k == 0) [1..n `div` 2]
