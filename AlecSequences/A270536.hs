3f4c5b609830a102f9486dbea8f3b984ceaf0ca9
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-21 11:04:26 -0700

    A270536 with test.

---
+++ AlecSequences/A270536.hs
@@ -0,0 +1,15 @@
+module AlecSequences.A270536 (a270536) where
+import Helpers.AlecHelper (buildAlecSequence)
+import Helpers.Primes (isPrime)
+import HelperSequences.A032741 (a032741)
+
+a270536 :: Int -> Integer
+a270536 i = a270536_list !! (i - 1)
+
+a270536_list :: [Integer]
+a270536_list = buildAlecSequence matchingIndices sum [0]
+
+matchingIndices :: [Integer] -> [Int]
+matchingIndices list = filter f [1..n] where
+  n = length list
+  f i = not $ isPrime $ toInteger n + 1 + a270536 i
