4d1774d69986f7687d91dc981dc031c77e20a832
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-22 10:51:30 -0700

    Widen input on A001057, A002061, A002522.

--- HelperSequences/A001057.hs
+++ HelperSequences/A001057.hs
@@ -1,4 +1,5 @@
 module HelperSequences.A001057 (a001057) where
 
-a001057 :: Integer -> Integer
-a001057 n = if even n then div n (-2) else div (n + 1) 2
+a001057 :: Integral a => a -> Integer
+a001057 n' = if even n then div n (-2) else div (n + 1) 2 where
+  n = toInteger n'
--- HelperSequences/A002522.hs
+++ HelperSequences/A002522.hs
@@ -1,4 +1,4 @@
 module HelperSequences.A002522 (a002522) where
 
-a002522 :: Integer -> Integer
-a002522 n = n^2 + 1
+a002522 :: Integral a => a -> Integer
+a002522 n = toInteger n ^ 2 + 1
