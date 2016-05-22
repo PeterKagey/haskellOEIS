6dfb9e8a5b5b6eaf0a5abf21f0f19fb80c1e6cbc
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-22 14:43:59 -0700

    Add helpers for base-b representation and lossless zipping.

--- Helpers/ListHelpers.hs
+++ Helpers/ListHelpers.hs
@@ -1,4 +1,4 @@
-module Helpers.ListHelpers (concatReplicate, reciprocalSum, runLengths) where
+module Helpers.ListHelpers (concatReplicate, reciprocalSum, runLengths, zipWithPadding) where
 -- concatReplicate is to replicate as concatMap is to map
 concatReplicate :: Int -> [a] -> [a]
 concatReplicate n list = take (n * length list) $ cycle list
@@ -12,3 +12,10 @@ runLengths :: Eq a => [a] -> [Int]
 runLengths [] = []
 runLengths l = h : runLengths (drop h l) where
   h = length $ takeWhile (== head l) l
+
+-- Inspired by:
+-- http://stackoverflow.com/questions/22403029/how-to-zip-lists-with-different-length
+zipWithPadding :: a -> [a] -> [a] -> [(a, a)]
+zipWithPadding pad (a:as) (b:bs) = (a, b) : zipWithPadding pad as bs
+zipWithPadding pad as     []      = zip as (repeat pad)
+zipWithPadding pad []      bs     = zip (repeat pad) bs
