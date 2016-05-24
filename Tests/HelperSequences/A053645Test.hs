f8bbe695437670b2b9acd29c1a7d4ff44db40af6
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-24 11:06:30 -0700

    A053645 with test.

---
+++ Tests/HelperSequences/A053645Test.hs
@@ -0,0 +1,10 @@
+module Tests.HelperSequences.A053645Test where
+import Test.Hspec
+import HelperSequences.A053645 (a053645)
+main :: IO ()
+
+main = hspec $
+  describe "A053645" $
+    it "correctly computes the first 20 elements" $
+      take 20 (map a053645 [1..]) `shouldBe` expectedValue where
+        expectedValue = [0,0,1,0,1,2,3,0,1,2,3,4,5,6,7,0,1,2,3,4]
