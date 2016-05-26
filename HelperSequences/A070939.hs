df5b1d9ff0db3542a49170b88080151dd0ce3957
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-25 23:36:15 -0700

    Refactor A070939.

--- HelperSequences/A070939.hs
+++ HelperSequences/A070939.hs
@@ -1,4 +1,6 @@
 module HelperSequences.A070939 (a070939) where
 
-  a070939 :: Int -> Int
-  a070939 n = if n < 2 then 1 else a070939 (n `div` 2) + 1
+a070939 :: Int -> Int
+a070939 n
+  | n < 2     = 1
+  | otherwise = a070939 (n `div` 2) + 1
