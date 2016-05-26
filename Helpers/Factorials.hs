da0879490296662c642585f51a5a071eccf3bbf6
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-05-25 23:33:43 -0700

    Factorial helper with `binomial`.

---
+++ Helpers/Factorials.hs
@@ -0,0 +1,13 @@
+module Helpers.Factorials (binomial) where
+
+
+factorial :: Integer -> Integer
+factorial n = factorial' n 1 where
+  factorial' 0 accum = accum
+  factorial' n accum = factorial' (n - 1) (accum * n)
+
+binomial :: Integer -> Integer -> Integer
+binomial _ 0 = 1
+binomial 0 _ = 0
+binomial n k = product [k' + 1..n] `div` factorial (n - k') where
+    k' = max k (n - k)
