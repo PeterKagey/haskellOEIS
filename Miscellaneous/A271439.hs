077567a6bb36619db1b74625e748312e5b760dbb
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-06-09 13:17:34 -0700

    a000217_list: triangular numbers as a list.

--- Miscellaneous/A271439.hs
+++ Miscellaneous/A271439.hs
@@ -1,10 +1,8 @@
 module Miscellaneous.A271439 (a271439) where
-import HelperSequences.A000217 (a000217)
+import HelperSequences.A000217 (a000217_list)
 import HelperSequences.A002024 (a002024)
 
-a271439 :: Int -> Integer
+a271439 :: Integral a => a -> a
 a271439 0 = 0
-a271439 n = if isTriangular n then 0 else n' - toInteger (a002024 n) + 1 where
-  isTriangular n = n' == last (takeWhile (<= n') a000217_list)
-  a000217_list = map a000217 [1..]
-  n' = toInteger n
+a271439 n = if isTriangular then 0 else n - a002024 n + 1 where
+  isTriangular = n == last (takeWhile (<= n) a000217_list)
