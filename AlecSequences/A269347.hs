0c519e425e76c8f69c261f1c937d01cec97fdb1c
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-06-27 16:04:07 -0700

    Speed up functions by tightening signatures
    I still don't know why memoizing doesn't seem to play nice with the wider signatures.

--- AlecSequences/A269347.hs
+++ AlecSequences/A269347.hs
@@ -1,15 +1,10 @@
 module AlecSequences.A269347 (a269347) where
-import Helpers.AlecHelper (buildAlecSequence)
-import Data.List (genericIndex, genericLength)
+import Data.List (genericIndex)
 
-a269347 :: Integral a => a -> a
-a269347 i = genericIndex a269347_list (i - 1)
+a269347 :: Integer -> Integer
+a269347 1 = 1
+a269347 n = genericIndex a269347_list (n - 1)
 
-a269347_list :: Integral a => [a]
-a269347_list = buildAlecSequence matchingIndices sum [1]
-
-matchingIndices :: Integral a => [a] -> [a]
-matchingIndices list = filter f [1..n - 1] where
-  n = genericLength list + 1
-  f index = n `mod` a_i == 0 where
-    a_i = genericIndex list (index - 1)
+a269347_list :: [Integer]
+a269347_list = map a [1..] where
+  a n = sum $ filter ((==) 0 . mod n . a269347) [1..n-1]
--- AlecSequences/A269526.hs
+++ AlecSequences/A269526.hs
@@ -1,6 +1,9 @@
 module AlecSequences.A269526 (a269526) where
 import Miscellaneous.A274080 (a274080_row)
-import Data.List ((\\))
+import Data.List ((\\), genericIndex)
 
-a269526 :: Integral a => a -> a
-a269526 n = head $ [1..] \\ map a269526 (a274080_row n)
+a269526 :: Integer -> Integer
+a269526 n = genericIndex a269526_list (n - 1)
+
+a269526_list = map f [1..] where
+  f n = head $ [1..] \\ map a269526 (a274080_row n)
