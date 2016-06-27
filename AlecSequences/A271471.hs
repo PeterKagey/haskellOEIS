392bdc63474d0e361e9e701c78c77fc179c4a9c4
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-06-27 16:04:46 -0700

    Refactor A271471.

--- AlecSequences/A271471.hs
+++ AlecSequences/A271471.hs
@@ -1,17 +1,10 @@
 module AlecSequences.A271471 (a271471) where
-import AlecSequences.A271468 (a271468)
+import AlecSequences.A271468 (a271468, a271468_list)
 import Data.Maybe (fromJust)
 import Data.List (find)
 
 a271471 :: Int -> Integer
 a271471 n = a271471_list !! (n - 1)
 
-a271471_list :: [Integer]
-a271471_list = 5 : remainingFrom 1 where
-  remainingFrom n = nextTerm : remainingFrom (n + 1) where
-    nextTerm = fromJust $ find f validTerms where
-      -- Part of the reason this is so slow is because this filter naively
-      -- starts at a271468(1), a271468(2), ...
-      validTerms = filter (\j -> j > a271471 n) $ map a271468 [1..]
-      f a_i = all notDivisibleBySmallerTerm $ take n a271471_list where
-        notDivisibleBySmallerTerm k = a_i `mod` k /= 0
+a271471_list = f $ tail a271468_list where
+  f (a:as) = a : f (filter (\i -> i `mod` a /= 0) as)
