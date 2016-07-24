39c32322b08f7ce766d7a630c3cbc365005fa379
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-07-24 12:24:21 -0700

    A274701 with test.

---
+++ Miscellaneous/A274701.hs
@@ -0,0 +1,9 @@
+module Miscellaneous.A274701 (a274701) where
+import Miscellaneous.A259280 (a259280)
+import Helpers.ListHelpers (firstDifferences)
+
+a274701 :: Int -> Integer
+a274701 n = a274701_list !! (n - 1)
+
+a274701_list :: [Integer]
+a274701_list = firstDifferences $ map a259280 [1..]
