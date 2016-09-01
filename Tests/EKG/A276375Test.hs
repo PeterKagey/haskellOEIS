c68fb7bdd1483cc0d62867ad6ab81d56224d2d5c
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-09-01 14:16:55 -0700

    A276375 with test and b-file.

---
+++ Tests/EKG/A276375Test.hs
@@ -0,0 +1,10 @@
+module Tests.EKG.A276375Test where
+import Test.Hspec
+import EKG.A276375 (a276375)
+main :: IO ()
+
+main = hspec $
+  describe "A276375" $
+    it "correctly computes the first 20 elements" $
+      take 20 (map a276375 [1..]) `shouldBe` expectedValue where
+        expectedValue = [1,2,3,11,22,29,47,49,52,67,78,79,80,91,103,104,109,111,121,130]
