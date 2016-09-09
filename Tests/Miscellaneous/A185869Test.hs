a5a71c4aa119390530d9dd96efb617ec930b47d8
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-09-09 15:47:30 -0700

    A185869 with test and b-file.

---
+++ Tests/Miscellaneous/A185869Test.hs
@@ -0,0 +1,10 @@
+module Tests.Miscellaneous.A185869Test where
+import Test.Hspec
+import Miscellaneous.A185869 (a185869)
+main :: IO ()
+
+main = hspec $
+  describe "A185869" $
+    it "correctly computes the first 20 elements" $
+      take 20 (map a185869 [1..]) `shouldBe` expectedValue where
+        expectedValue = [2,7,9,16,18,20,29,31,33,35,46,48,50,52,54,67,69,71,73,75]
