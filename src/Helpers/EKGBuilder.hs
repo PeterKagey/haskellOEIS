87f744e60711b9af5ffe3aeba9c9f29fd137c68b
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-09-15 15:47:35 -0700

    narrow type signatures of some EKG sequences

--- src/Helpers/EKGBuilder.hs
+++ src/Helpers/EKGBuilder.hs
@@ -1,14 +1,9 @@
 module Helpers.EKGBuilder (buildEKG) where
 
-import Data.List (find, (\\))
+import Data.List (find, delete, (\\))
 import Data.Maybe (fromJust)
 
 buildEKG :: Integral a => [a] -> [a]
-buildEKG seed = ekg_list where
-  ekg_list = seed ++ remaining_list (length seed) where
-    remaining_list k = next_term : remaining_list (k + 1) where
-      next_term = fromJust $ find nonCoprime unrepresentedInts where
-        -- unrepresentedInts is the performance bottle-neck.
-        unrepresentedInts = [1..] \\ take k ekg_list
-        nonCoprime i = gcd lastTerm i /= 1 where
-          lastTerm = ekg_list !! (k - 1)
+buildEKG seed = seed ++ ekg (last seed) ([1..] \\ seed) where
+  ekg k l = k' : ekg k' (delete k' l) where
+    k' = fromJust $ find ((1 /=) . gcd k) l
