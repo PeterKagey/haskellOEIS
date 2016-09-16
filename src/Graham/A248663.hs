1e41ca50b6af6d51f2919a16fcead06d778fb10c
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-09-16 13:30:22 -0700

    Clean `a248663`.

--- src/Graham/A248663.hs
+++ src/Graham/A248663.hs
@@ -1,15 +1,6 @@
 module Graham.A248663 (a248663) where
-import Helpers.Primes (isPrime)
+import HelperSequences.A112798 (a112798_row)
 import Data.Bits (xor)
-import Data.Maybe (fromJust)
-import Data.List (find)
-import HelperSequences.A000040 (a000040_list)
 
 a248663 :: Integer -> Integer
-a248663 n
-  | n <= 1    = 0
-  | isPrime n = 2^e
-  | otherwise = a248663 a `xor` a248663 b where
-    a = fromJust $ find (\d -> n `mod` d == 0) [2..]
-    b = n `div` a
-    e = fromJust $ find (\i -> a000040_list !! i == n) [0..]
+a248663 = foldr (xor) 0 . map (\i -> 2^(i - 1)) . a112798_row
