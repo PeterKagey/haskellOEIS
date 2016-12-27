65778970fb6d1676c47bf92b662fd3601156bc9e
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-12-27 11:45:11 -0800

    Add AnagramHelper.

--- src/Miscellaneous/A279688.hs
+++ src/Miscellaneous/A279688.hs
@@ -1,5 +1,5 @@
 module Miscellaneous.A279688 (a279688, a279688_list) where
-import Helpers.AnagramHelper (possibleAnagramBases, isBaseBAnagram)
+import Helpers.AnagramHelper (possibleAnagramBases, nIsBaseBAnagramOf2n)
 
 a279688 :: Int -> Integer
 a279688 n = a279688_list !! (n - 1)
@@ -7,5 +7,6 @@ a279688 n = a279688_list !! (n - 1)
 a279688_list :: [Integer]
 a279688_list = 0 : filter isAnyAnagram [1..]
 
+-- There exists some base such that n & 2n are anagrams.
 isAnyAnagram :: Integer -> Bool
-isAnyAnagram n = any (isBaseBAnagram n) $ possibleAnagramBases n
+isAnyAnagram n = any (nIsBaseBAnagramOf2n n) $ possibleAnagramBases n
