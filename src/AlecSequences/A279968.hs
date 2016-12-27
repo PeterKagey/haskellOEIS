2ce3252c1e84a0b109015f7120c2c0124f32b184
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-12-27 11:45:31 -0800

    Simplify A279968.

--- src/AlecSequences/A279968.hs
+++ src/AlecSequences/A279968.hs
@@ -3,10 +3,9 @@ import Miscellaneous.A274080 (a274080_row)
 import Data.List (genericIndex, genericLength)
 
 a279968 :: Integer -> Integer
-a279968 1 = 0
 a279968 n = genericIndex a279968_list (n - 1)
 
 a279968_list :: [Integer]
-a279968_list = 0 : map count [2..] where
+a279968_list = map count [1..] where
   count n = genericLength $ filter (odd . (n+)) adjacentLabels where
     adjacentLabels = map a279968 (a274080_row n)
