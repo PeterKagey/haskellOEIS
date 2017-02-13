30e31536cb50d7e28995366a40c70ffa058fce7b
Peter Kagey <pkagey@sweetspotdiabetes.com>
2017-02-13 14:33:32 -0800

    Fix mismatched function name.

--- src/Miscellaneous/A279916.hs
+++ src/Miscellaneous/A279916.hs
@@ -1,11 +1,11 @@
 module Miscellaneous.A279916 (a279916, a279916_list) where
 import Data.Maybe (fromMaybe)
 import Data.List (find)
-import Helpers.AnagramHelper (possibleAnagramBases, isBaseBAnagram)
+import Helpers.AnagramHelper (possibleAnagramBases, nIsBaseBAnagramOf2n)
 import Miscellaneous.A279688 (a279688)
 
 a279916 :: Int -> Integer
-a279916 n = fromMaybe 0 $ find (isBaseBAnagram a_n) bases  where
+a279916 n = fromMaybe 0 $ find (nIsBaseBAnagramOf2n a_n) bases  where
   bases = possibleAnagramBases a_n
   a_n = a279688 n
 
