d3c8eedab6f9539ae5035c772c557fa1d2f12153
Peter Kagey <peterkagey@gmail.com>
2017-03-26 21:27:08 -0700

    A283557 with test.

--- src/Helpers/BraxtonHelper.hs
+++ src/Helpers/BraxtonHelper.hs
@@ -1,13 +1,24 @@
-module Helpers.BraxtonHelper (allSequences, SymmetricRelation(..), ReflexiveRelation(..)) where
+module Helpers.BraxtonHelper (allSequences, enumerateSequences, SymmetricRelation(..), ReflexiveRelation(..)) where
 
 data SymmetricRelation = Symmetric | NonSymmetric deriving (Eq)
 data ReflexiveRelation = Reflexive | NonReflexive deriving (Eq)
 
 allSequences :: ReflexiveRelation -> SymmetricRelation -> Int -> Int -> [[Int]]
-allSequences reflexivity symmetry upperBound maxLength = f [[]] maxLength where
+allSequences reflexivity symmetry upperBound = f [[]] where
   f knownSequences 0 = knownSequences
   f knownSequences stepCount = f newSequences (stepCount - 1) where
     newSequences = concatMap (childSequences reflexivity symmetry upperBound) knownSequences
+
+enumerateSequences :: ReflexiveRelation ->
+                      SymmetricRelation ->
+                      ([Integer] -> Integer) -> -- e.g. `product`, `sum`, `maximum`
+                      Int ->                    -- upper bound
+                      Int ->                    -- sequence length
+                      [(Integer, [Integer])]    -- number of sequences
+enumerateSequences reflexivity symmetry f upperBound numberOfTerms = filter ((==minReduction) . fst) reductionTuples where
+  reductionTuples = map (\s -> (f s, s)) theSequences
+  minReduction = minimum $ map fst reductionTuples
+  theSequences = map (map fromIntegral) $ allSequences reflexivity symmetry upperBound numberOfTerms
 
 childSequences :: ReflexiveRelation -> SymmetricRelation -> Int -> [Int] -> [[Int]]
 childSequences reflexivity symmetry upperBound as = map (:as) validTerms where
@@ -17,5 +28,5 @@ isValidExtension :: ReflexiveRelation -> SymmetricRelation -> [Int] -> Int -> Bo
 isValidExtension _ _ [] _ = True
 isValidExtension reflexivity symmetry as@(a:t) c = checkSymmetric && checkReflexive where
   checkSymmetric = (symmetry == NonSymmetric) || a /= c
-  checkReflexive = (all validNeighbor $ zip as t) where
+  checkReflexive = all validNeighbor $ zip as t where
     validNeighbor pair = pair /= (c, a) && (reflexivity == NonReflexive || pair /= (a, c))
