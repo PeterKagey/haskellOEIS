8e3273730095b0b7b7f05f0e65f22a2c22a2b826
Peter Kagey <peterkagey@gmail.com>
2017-03-27 18:22:01 -0700

    Fix wrong reference in test.

--- test/Braxton/A284435Spec.hs
+++ test/Braxton/A284435Spec.hs
@@ -1,6 +1,6 @@
 module Braxton.A284435Spec (main, spec) where
 import Test.Hspec
-import Braxton.A284435 (a283558)
+import Braxton.A284435 (a284435)
 
 main :: IO ()
 main = hspec spec
@@ -8,5 +8,5 @@ main = hspec spec
 spec :: Spec
 spec = describe "A284435" $
   it "correctly computes the first 5 elements" $
-    take 5 (map a283558 [1..]) `shouldBe` expectedValue where
+    take 5 (map a284435 [1..]) `shouldBe` expectedValue where
       expectedValue = [1,1,2,2,4]
