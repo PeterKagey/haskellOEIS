8ab1aab9964272b7ccb7220799f167118fc7d074
Peter Kagey <peterkagey@gmail.com>
2017-03-27 18:21:28 -0700

    Correct A214674 offset (1, not 0).

--- src/SubprimeFib/A214674.hs
+++ src/SubprimeFib/A214674.hs
@@ -2,7 +2,7 @@ module SubprimeFib.A214674 (a214674) where
 import Helpers.SubprimeFib (subprimeFib)
 
 a214674 :: Int -> Integer
-a214674 n = a214674_list !! n
+a214674 n = a214674_list !! (n - 1)
 
 a214674_list :: [Integer]
 a214674_list = subprimeFib 1 1
