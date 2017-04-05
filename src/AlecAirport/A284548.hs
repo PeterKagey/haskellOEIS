4e9a5e7e4cd43c3a402ef2bc3f2650d8027e947f
Peter Kagey <peterkagey@gmail.com>
2017-04-05 13:37:08 -0700

    A284548 with test.

---
+++ src/AlecAirport/A284548.hs
@@ -0,0 +1,20 @@
+module AlecAirport.A284548 (a284548) where
+import Data.List (find)
+import Data.Maybe (fromJust)
+import Data.Ratio ((%))
+
+a284548_list = alecAirportHelper div
+
+a284548 = (!!) a284548_list
+
+alecAirportHelper :: (Int -> Int -> Int) -> [Int]
+alecAirportHelper f = 1 : remaining 1 (repeat []) [Just 1] where
+  remaining :: Int -> [[Int]] -> [Maybe Int] -> [Int]
+  remaining n (l:ls) knownTerms = a_n : remaining (n + 1) nextGeneration (Just a_n : knownTerms) where
+    a_n = fromJust $ find (not . (`elem` l)) [1..]
+    nextGeneration = zipWith optionalCons termsToMarkOff ls where
+      termsToMarkOff = take (f n a_n) knownTerms ++ repeat Nothing
+
+optionalCons :: Maybe a -> [a] -> [a]
+optionalCons (Just a) as = a : as
+optionalCons _ as = as
