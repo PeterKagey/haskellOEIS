356acd77129e7c7897e905b73b7965abbcb34d6e
Peter Kagey <peterkagey@gmail.com>
2017-09-26 22:21:35 -0700

    Minor improvement of algorithm.

--- src/StoneTransfer/A292726.hs
+++ src/StoneTransfer/A292726.hs
@@ -1,34 +1,29 @@
 module StoneTransfer.A292726 (a292726, a292726_list) where
 import Data.List (delete, nub, sort, null)
 import Data.Set (Set, empty, insert, notMember)
+import Data.IntMap.Strict (IntMap, toList, adjust, fromList)
 -- The best structure for this would probably be a Scala-like Map[Int, Int],
 -- which behaves like a histogram.
 -- This is much slower than the Ruby implementation.
 
-a292726 n = length $ enumerateFrom $ replicate n 1
+a292726 n = length $ enumerateFrom $ fromList $ (1, n) : map (\i -> (i, 0)) [2..n]
 a292726_list = map a292726 [1..]
 
-count :: Eq a => a -> [a] -> Int
-count a = length . filter (== a)
+type State = IntMap Int
 
-multiples :: [Int] -> [Int]
-multiples as = filter (\a -> count a as > 1) $ nub as
+-- Given a state return all possible child states. For example:
+-- nextGeneration (3, 3, 2, 1, 1)
+-- => [(6, 2, 1, 1), (5, 2, 1, 1, 1), (4, 2, 2, 1, 1), (3, 3, 2, 2)]
+nextGeneration :: State -> [State]
+nextGeneration currentGen = concatMap (`descendants` currentGen) $ multiples currentGen where
+  multiples as = map fst $ filter (\(_, count) -> count >= 2) $ toList as
+  descendants pileSize currentGen = map (transferStones pileSize currentGen) [1..pileSize] where
+    transferStones pileSize currentGen n = foldr (uncurry adjust) currentGen changeSet where
+      changeSet = [(subtract 2, pileSize), ((+1), pileSize - n), ((+1), pileSize + n)]
 
-descendants :: Int -> [Int] -> [[Int]]
-descendants pileSize currentGen = map (transferStones pileSize currentGen) [1..pileSize]
-
-transferStones :: Int -> [Int] -> Int -> [Int]
-transferStones pileSize currentGen numberToTransfer = sort $ filter (>0) $ biggerPile : smallerPile : reducedGroup where
-  biggerPile = pileSize + numberToTransfer
-  smallerPile = pileSize - numberToTransfer
-  reducedGroup = delete pileSize (delete pileSize currentGen)
-
-nextGeneration :: [Int] -> [[Int]]
-nextGeneration currentGen = concatMap (`descendants` currentGen) $ multiples currentGen
-
-enumerateFrom :: [Int] -> Set [Int]
+enumerateFrom :: State -> Set State
 enumerateFrom initialGeneration = recurse [initialGeneration] empty where
-  recurse :: [[Int]] -> Set [Int] -> Set [Int]
+  recurse :: [State] -> Set State -> Set State
   recurse currentGen ledger = if null newGen then updatedLedger else recurse newGen updatedLedger where
     newGen = filter (`notMember` ledger) $ concatMap nextGeneration currentGen
     updatedLedger = insertList currentGen ledger
