37a258cbd8b6444d78fc0e7e13dcb9be8897bdde
Peter Kagey <peterkagey@gmail.com>
2019-03-18 13:01:38 -0700

    A285521: Fix small typo in test description.

--- test/ConvexPolygon/A285521Spec.hs
+++ test/ConvexPolygon/A285521Spec.hs
@@ -7,6 +7,6 @@ main = hspec spec
 
 spec :: Spec
 spec = describe "A285521" $
-  it "correctly computes the first 5 elements" $
+  it "correctly computes the first 20 elements" $
       map a285521 [1..20] `shouldBe` expectedValue where
         expectedValue = [1,1,1,1,1,2,1,1,2,2,1,1,3,2,3,1,2,1,3,2]
