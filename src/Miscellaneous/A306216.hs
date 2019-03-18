91ba3a272d4a5708222ecb8cc9ff5a112535bc53
Peter Kagey <peterkagey@gmail.com>
2019-03-18 16:38:03 -0700

    Move program to be comment in another sequence.

--- src/Miscellaneous/A306216.hs
+++ src/Miscellaneous/A306216.hs
@@ -10,3 +10,17 @@ a306216_list = 1 : 1 : concat (unfoldr nextGeneration [1,1]) where
 
 a306216 :: Int -> Int
 a306216 n = a306216_list !! (n-1)
+
+-- Another variant: 
+-- import Data.List (unfoldr)
+--
+-- weaklyDecreasing :: [Int] -> [Int]
+-- weaklyDecreasing [] = []
+-- weaklyDecreasing (n:ns) = recurse 1 n ns where
+--   recurse c m [] = [c]
+--   recurse c m (k:ks)
+--     | k <= m    = recurse (c+1) k ks
+--     | otherwise = c : recurse 1 k ks
+--
+-- a999999_list = 1 : concat (unfoldr nextGeneration [1]) where
+--   nextGeneration l = Just (weaklyDecreasing l, l ++ weaklyDecreasing l)
