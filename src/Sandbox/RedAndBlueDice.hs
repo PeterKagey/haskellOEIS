73021f3d2138bec45b8fc77472e3f07c14bf3a4d
Peter Kagey <peterkagey@gmail.com>
2019-03-18 17:11:16 -0700

    Add comments to Sandbox file.

--- src/Sandbox/RedAndBlueDice.hs
+++ src/Sandbox/RedAndBlueDice.hs
@@ -1,5 +1,6 @@
 module Sandbox.RedAndBlueDice where
 import Data.List (permutations, tails)
+import Helpers.Records (allMax, allMin, countMax, countMin, maxIndices)
 -- https://math.stackexchange.com/questions/3035452/error-in-solution-of-peter-winkler-red-and-blue-dice-puzzle
 -- On permutations
 
@@ -30,3 +31,20 @@ permutationPairs n = [equalSumSubstrings a b | a <- permutations [1..n], b <- pe
 
 -- Fewest number of nonempty substrings in a, b in S_n with equal sums.
 -- 1,3,6,8,11,15
+
+-- Greatest number of nonempty substrings in a, b in S_n with equal sums.
+-- 1,3,8,16,27
+
+-- Pairs (a, b) \in S_n \times S_n such that the number of nonempty substrings in a and b is maximized.
+-- a' n = countMax (\(a, b) -> equalSumSubstrings a b) $ permutationPairs' n
+-- 1,4,8,4,8
+-- Example for n=5
+-- (52143,52143),(52143,34125),(51324,51324),(51324,42315),(34125,52143),(34125,34125),(42315,51324),(42315,42315)
+-- How many distinct permutations show up? In this example, just four (of which two are essentially different): 52143, 51324, 42315, 34125
+
+-- Pairs (a, b) \in S_n \times S_n such that the number of nonempty substrings in a and b is minimized.
+-- a' n = countMin (\(a, b) -> equalSumSubstrings a b) $ permutationPairs' n
+-- 1,4,20,24,48
+-- Example for n = 4
+-- (3412,4231),(3412,1324),(1432,4213),(1432,3124),(4132,1243),(4132,3421),(2143,4231),(2143,1324),(4213,1432),(4213,2341),(1243,4132),(1243,2314),(2341,4213),(2341,3124),(4231,3412),(4231,2143),(3421,4132),(3421,2314),(1324,3412),(1324,2143),(3124,1432),(3124,2341),(2314,1243),(2314,3421])
+-- Twelve distinct permutations: 1243,1324,1432,2143,2314,2341,3124,3412,3421,4231,4213,4132
