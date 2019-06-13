b6b52ff8996dcf99157c920af418dc1cea418d97
Peter Kagey <peterkagey@gmail.com>
2019-06-13 16:44:31 -0700

    Add bitsList function to Binary Helper.

--- src/Helpers/Binary.hs
+++ src/Helpers/Binary.hs
@@ -1,7 +1,15 @@
-module Helpers.Binary (lastBits) where
+module Helpers.Binary (lastBits, bitsList) where
 
 -- Number of partitions of the binary expansion of n into consecutive blocks with no leading zeroes.
 lastBits :: Int -> [Int]
 lastBits n = recurse 0 n where
   recurse _ 0 = []
   recurse k n' = (n `mod` 2^k) : recurse (k + 1) (div n' 2)
+
+bitsList :: Integer -> [Bool]
+bitsList 0 = [False]
+bitsList n = recurse n [] where
+  recurse n' b
+    | n' == 0 = b
+    | even n' = recurse (n' `div` 2) (False : b)
+    | odd n'  = recurse (n' `div` 2) (True : b)
