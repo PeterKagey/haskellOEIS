a3518871c3fee73364e9644d6a8a76d89aed959c
Peter Kagey <peterkagey@gmail.com>
2019-10-07 18:48:47 -0700

    A327743 with test.

---
+++ src/KthDifferences/A327743.hs
@@ -0,0 +1,24 @@
+module KthDifferences.A327743 (a327743_list, a327743) where
+import Data.Set (Set, singleton, insert, member)
+import Data.Maybe (Maybe, mapMaybe)
+
+type KthDifferences = [(Integer, Set Integer)]
+
+-- Each list of k-th differences distinct.
+a327743_list :: [Integer]
+a327743_list = recurse [] where
+  recurse kthDifferences = n : recurse ds where
+    ds@((n,_):_) = nextDifferences kthDifferences
+
+a327743 :: Int -> Integer
+a327743 n = a327743_list !! (n - 1)
+
+nextDifferences :: KthDifferences -> KthDifferences
+nextDifferences ds = head $ mapMaybe (`updateDifferences` ds) [1..]
+
+updateDifferences :: Integer -> KthDifferences -> Maybe KthDifferences
+updateDifferences nextTerm [] = Just [(nextTerm, singleton nextTerm)]
+updateDifferences nextTerm ((d, ds):ds')
+  | nextTerm `member` ds = Nothing
+  | otherwise            = fmap (updated:) (updateDifferences (nextTerm - d) ds') where
+    updated = (nextTerm, insert nextTerm ds)
