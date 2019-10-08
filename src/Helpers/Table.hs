7b4a97df0ac1417fecc74f600dc41c4b91bfe210
Peter Kagey <peterkagey@gmail.com>
2019-10-08 10:44:27 -0700

    Update table helper for A327844.

--- src/Helpers/Table.hs
+++ src/Helpers/Table.hs
@@ -1,4 +1,4 @@
-module Helpers.Table (n_k, n'_k') where
+module Helpers.Table (n_k, n'_k', indicesByAntidiagonals) where
 import HelperSequences.A025581 (a025581)
 import HelperSequences.A002262 (a002262)
 
@@ -12,3 +12,13 @@ n_k i = (a025581 i, a002262 i)
 n'_k' :: Integral a => a -> (a, a)
 n'_k' i = (n + k, k) where
   (n, k) = n_k i
+
+-- (0,0),(1,0),(0,1),(2,0),(1,1),(0,2),(3,0),(2,1),(1,2),(0,3)
+-- (0,0),(0,1),(0,2),(0,3)
+-- (1,0),(1,1),(1,2),
+-- (2,0),(2,1),
+-- (3,0),
+indicesByAntidiagonals :: Integral a => [(a, a)]
+indicesByAntidiagonals = recurse (0,0) where
+  recurse i@(0, a) = i : recurse (a + 1, 0)
+  recurse i@(a, b) = i : recurse (a - 1, b + 1)
