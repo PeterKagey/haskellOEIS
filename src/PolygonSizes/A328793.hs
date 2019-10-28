70820bfa2d6e885bd26adf86578b6a45dee6ce47
Peter Kagey <peterkagey@gmail.com>
2019-10-28 10:20:31 -0700

    A328793 with test.
    	# A003136 as helper sequence.

---
+++ src/PolygonSizes/A328793.hs
@@ -0,0 +1,13 @@
+module PolygonSizes.A328793 (a328793) where
+import Data.List (findIndices)
+import Data.Set (member)
+import Helpers.PolygonSizes (triangleSizes)
+import HelperSequences.A003136 (a003136)
+
+-- This could be a little faster by using the fact that
+-- A328793(A024610(n) + k) > n + 1 for all n > 0, k >= 0.
+a328793 :: Int -> Int
+a328793 n = 2 + head (findIndices (a003136 n `member`) triangleSizes)
+
+a328793_list :: [Int]
+a328793_list = map a328793 [2..]
