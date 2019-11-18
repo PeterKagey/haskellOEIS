a8d74fbc518a2e5d5411d4946445f3e3af1c855f
Peter Kagey <peterkagey@gmail.com>
2019-11-17 23:55:31 -0800

    A329479 with test.
    	# Helper functions A024493 and A024495.
    	# Add case for binomial n k when k < 0 or k > n.

--- src/Helpers/Factorials.hs
+++ src/Helpers/Factorials.hs
@@ -9,5 +9,7 @@ factorial n = factorial' n 1 where
 binomial :: Integral a => a -> a -> a
 binomial _ 0 = 1
 binomial 0 _ = 0
-binomial n k = product [k' + 1..n] `div` factorial (n - k') where
+binomial n k
+  | n < k'    = 0
+  | otherwise = product [k' + 1..n] `div` factorial (n - k') where
     k' = max k (n - k)
