0873099decacf007f27c672060a3632c27d3ae90
Peter Kagey <peterkagey@gmail.com>
2019-11-17 23:58:16 -0800

    Fix test descriptions.

--- test/Polynomial/A300002Spec.hs
+++ test/Polynomial/A300002Spec.hs
@@ -7,6 +7,6 @@ main = hspec spec
 
 spec :: Spec
 spec = describe "A300002" $
-  it "correctly computes the first 20 elements" $
+  it "correctly computes the first 8 elements" $
     take 8 (map a300002 [1..]) `shouldBe` expectedValue where
       expectedValue = [1, 2, 4, 3, 6, 5, 9, 16]
--- test/Polynomial/A301802Spec.hs
+++ test/Polynomial/A301802Spec.hs
@@ -7,6 +7,6 @@ main = hspec spec
 
 spec :: Spec
 spec = describe "A301802" $
-  it "correctly computes the first 20 elements" $
+  it "correctly computes the first 6 elements" $
     take 6 (map a301802 [1..]) `shouldBe` expectedValue where
       expectedValue = [1, 2, 4, 18, 48, 216]
