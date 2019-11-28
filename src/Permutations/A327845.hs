724ca28c1a14e5bd2fe79ab298c3e46ce6330612
Peter Kagey <peterkagey@gmail.com>
2019-11-27 18:30:40 -0800

    A327845 with test.

---
+++ src/Permutations/A327845.hs
@@ -0,0 +1,11 @@
+module Permutations.A327845 where
+import Helpers.ListHelpers (firstDifferences, allDistinct)
+import Data.List (permutations)
+import Data.Set (empty, insert, member)
+
+distinctDifferences :: Integral a => [a] -> Bool
+distinctDifferences [] = True
+distinctDifferences l = allDistinct l && distinctDifferences (firstDifferences l)
+
+a327845 :: Int -> Int
+a327845 n = length $ filter (distinctDifferences . firstDifferences) $ permutations [1..n]
