2b245634298e07475469d48aa208bee3e2bf66ca
Peter Kagey <peterkagey@gmail.com>
2019-11-27 19:39:32 -0800

    Fix Grasshopper.hs & rename.

--- src/Helpers/Grasshopper.hs
+++ src/Helpers/Grasshopper.hs
@@ -1,4 +1,4 @@
-module Helpers.Grasshopper (grasshopper, grasshopper'', Direction (..)) where
+module Helpers.Grasshopper (grasshopper, Direction (..)) where
 import Data.Vector ((!), (//))
 import qualified Data.Vector as Vec
 import Control.Monad (mplus)
@@ -9,8 +9,8 @@ data Direction = Clockwise | Counterclockwise deriving (Eq)
 -- 1) Data about the *process* (e.g. how many Counterclockwise steps?)
 -- 2) Data about the *result* (e.g. what position is visited the most?)
 -- This function is best at computing *process* data.
-grasshopper'' :: a -> (Direction -> Vec.Vector Int -> a -> a) -> Int -> a
-grasshopper'' a f n = recurse a 1 0 $ Vec.replicate n 0 where
+grasshopper :: a -> (Direction -> Vec.Vector Int -> a -> a) -> Int -> a
+grasshopper a f n = recurse a 1 0 $ Vec.replicate n 0 where
   recurse a' stepCount pos hops
     | Vec.all (> 0) hops = a'
     | otherwise          = recurse a'' (stepCount + 1) pos' hops' where
