b7db511fcb91687d80af7df76c0673d19d3e59c2
Peter Kagey <peterkagey@gmail.com>
2020-03-20 09:41:57 -0700

    Add notes/examples to Table.hs.

--- src/Helpers/Table.hs
+++ src/Helpers/Table.hs
@@ -4,11 +4,15 @@ import HelperSequences.A002262 (a002262)
 
 -- 0 <= k; 0 <= n
 -- n_k 0 = (0, 0)
+-- [(0,0),(1,0),(0,1),(2,0),(1,1),(0,2),(3,0),(2,1),(1,2),(0,3),(4,0)]
+-- Table by antidiagonals
 n_k :: Integral a => a -> (a, a)
 n_k i = (a025581 i, a002262 i)
 
 -- 0 <= k <= n
 -- n_k 0 = (0, 0)
+-- [(0,0),(1,0),(1,1),(2,0),(2,1),(2,2),(3,0),(3,1),(3,2),(3,3),(4,0)]
+-- Triangle by rows
 n'_k' :: Integral a => a -> (a, a)
 n'_k' i = (n + k, k) where
   (n, k) = n_k i
