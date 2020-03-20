a9ef9e11c09f2863c68929445b1b86c772dbfd99
Peter Kagey <peterkagey@gmail.com>
2020-03-20 11:18:40 -0700

    A292997 with test.

---
+++ src/Subsets/A292997.hs
@@ -0,0 +1,29 @@
+module Subsets.A292997 (a292997) where
+import Data.Set (Set, (\\), delete, empty, fromList)
+import Helpers.Subsets (eachPair, combinations)
+import Helpers.Factorials (binomial, factorial)
+
+a292997 n = a292997_list !! (n - 1)
+
+a292997_list = concatMap a292997_row [1..]
+
+-- Can exploit the fact that each row is weakly longer than the preceding row.
+a292997_row n = head $ filter (`isValidSubset` s) ls where
+  s = fromList [1..n]
+  ls = canonicallyOrderedSubsequences [1..n]
+
+-- Assumes list is increasing
+isValidSubset :: [Int] -> Set Int -> Bool
+isValidSubset list targetSet = recurse pairs targetSet' where
+  pairs = eachPair list
+  targetSet' = targetSet \\ fromList list
+  recurse ((a,b):ls) s
+    | null s    = True
+    | otherwise = recurse ls (delete (b - a) $ delete (a + b) s)
+  recurse [] s
+    | null s = True
+    | otherwise = False
+
+-- Ordered by length, then lexicographically.
+-- Only makes sense for finite lists
+canonicallyOrderedSubsequences l = concatMap (`combinations` l) [0..length l]
