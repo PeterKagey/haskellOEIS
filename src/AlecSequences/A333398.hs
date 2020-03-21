784945ab86baeebf7fcb46a04fa4ad7616fd6b1f
Peter Kagey <peterkagey@gmail.com>
2020-03-20 23:46:36 -0700

    Remove old comment.

--- src/AlecSequences/A333398.hs
+++ src/AlecSequences/A333398.hs
@@ -3,7 +3,6 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.List (delete, transpose)
 
--- The "right" definition for this sequence
 a333398_list = 0 : recurse 0 (Set.singleton 0) (concat $ transpose [[-1,-2..],[1..]]) where
     recurse a previousTerms diffs = a' : recurse a' (Set.insert a' previousTerms) (delete d' diffs) where
         d' = head $ filter (\d -> (a + d) `Set.notMember` previousTerms) diffs
