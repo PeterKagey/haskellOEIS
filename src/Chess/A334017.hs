78ad08ad0fdf742b09ca158e0f0ff6d3feea6ae8
Peter Kagey <peterkagey@gmail.com>
2020-04-13 13:04:45 -0700

    A334017 with test.

---
+++ src/Chess/A334017.hs
@@ -0,0 +1,12 @@
+module Chess.A334017 (a334017) where
+import Helpers.ChessSequences (chessMoveCounter, queenN, queenW, queenSW)
+import Helpers.Table (n_k)
+
+a334017 :: Integer -> Integer
+a334017 n = case n_k (n - 1) of (n', k') -> a334017T  (n' + 1) (k' + 1)
+
+-- Longest chain is given by A094727.
+-- King analog: A033877
+a334017T :: Integer -> Integer -> Integer
+a334017T = chessMoveCounter visibleCells where
+    visibleCells n k = concatMap (\f -> f n k) [queenN, queenW, queenSW]
