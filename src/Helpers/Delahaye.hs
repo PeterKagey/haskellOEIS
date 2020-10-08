790cb13dd02f5c1803987fa36aeb5242381e548f
Peter Kagey <peterkagey@gmail.com>
2020-10-08 00:59:31 -0700

    Delahaye helper.

---
+++ src/Helpers/Delahaye.hs
@@ -0,0 +1,23 @@
+module Helpers.Delahaye (column, delahayeList, rowTable, antidiagonalTable) where
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Helpers.Table (tableByAntidiagonals, triangleByRows)
+
+type ExtensionCriteria a = [Integer] -> a -> Integer -> Bool
+type TableUpdate a = Integer -> [Integer] -> a -> a
+
+delahayeList :: Integer -> a -> ExtensionCriteria a -> TableUpdate a -> [Integer]
+delahayeList k initialTable isValidExtension updateTable = thisList where
+  thisList = k : recurse 1 initialTable where
+    recurse n table = nextTerm : recurse (n+1) (updateTable nextTerm knownTerms table) where
+      knownTerms = take n thisList
+      nextTerm = head $ filter (isValidExtension knownTerms table) [maximum knownTerms + 1 ..]
+
+column :: (Integer -> Integer -> Integer) -> [Integer] -> Integer -> Set Integer
+column f knownTerms k = Set.fromList (f k k : map (f k) knownTerms)
+
+rowTable :: (Integer -> Integer -> Integer) -> (Int -> Integer) -> [Integer]
+rowTable f aNumber = map ((\(i, j) -> f (aNumber (i + 1)) (aNumber (j + 1))) . triangleByRows) [0..]
+
+antidiagonalTable :: (Integer -> Integer -> Integer) -> (Int -> Integer) -> [Integer]
+antidiagonalTable f aNumber = map ((\(i, j) -> f (aNumber (i + 1)) (aNumber (j + 1))) . tableByAntidiagonals) [0..]
