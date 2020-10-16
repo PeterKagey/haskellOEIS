52a0aead5b5297e9a7425b8375354505cf306e9e
Peter Kagey <peterkagey@gmail.com>
2020-10-16 10:34:30 -0700

    A338201 with test.

---
+++ src/IntegerTriangles/A338201.hs
@@ -0,0 +1,7 @@
+module IntegerTriangles.A338201 (a338201, a338201_list) where
+import IntegerTriangles.A338202 (a338202_list)
+import Helpers.ListHelpers (firstDifferences)
+
+a338201 n = a338201_list !! (n - 1)
+
+a338201_list = 0 : firstDifferences a338202_list
