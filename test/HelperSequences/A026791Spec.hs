b61bd554ec24c7e47b348b6f880ce7fcc82aa250
Peter Kagey <peterkagey@gmail.com>
2020-10-19 22:42:05 -0700

    Fix A026791 test.

--- test/HelperSequences/A026791Spec.hs
+++ test/HelperSequences/A026791Spec.hs
@@ -9,4 +9,4 @@ spec :: Spec
 spec = describe "A026791" $
   it "correctly computes the first 20 elements" $
     take 20 (map a026791 [1..]) `shouldBe` expectedValue where
-      expectedValue = [	1,1,1,2,1,1,1,1,2,3,1,1,1,1,1,1,2,1,3]
+      expectedValue = [1,1,1,2,1,1,1,1,2,3,1,1,1,1,1,1,2,1,3,2]
